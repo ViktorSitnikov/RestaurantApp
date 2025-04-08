949c43911bf75884001383b2d23e1675fd2c01e2
viktorsitn <vit.sit2004@mail.ru>
2025-04-09 02:21:40 +0500

    Переделал функционал модального окна
    Теперь на странице всего одно модальное окно, в которое подгружаются данные с помощью js файла, вместо отдельных модальных окон для каждой позиции

---
+++ menu/static/menu/js/modal.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('[data-bs-toggle="modal-product"]').forEach(div => {
+        div.addEventListener('click', function() {
+            const productId = this.getAttribute('data-product-id');
+            const product = JSON.parse(this.getAttribute('data-product-json'));
+
+            document.getElementById('productModalLabel').textContent = product.name;
+            document.getElementById('modalProductImage').src = "/static/menu/img/" + product.photolink + ".jpg";
+            document.getElementById('modalProductDescription').textContent = product.description;
+            document.getElementById('modalProductPrice').textContent = product.price + "₽";
+            document.getElementById('modalProductGrams').textContent = product.grams + "г";
+            document.getElementById('modalProductStructure').textContent = product.structure;
+            document.getElementById('modalProductNutrition').innerHTML = `
+            Калории: ${product.kalories} ккал<br>
+            Белки: ${product.protein} ккал<br>
+            Жиры: ${product.fat} ккал<br>
+            Углеводы: ${product.carbohydrates} ккал<br>
+            `;
+            document.getElementById('modalProductPriceBtn').textContent = product.price;
+
+            const modal = new bootstrap.Modal(document.getElementById('productModal'));
+            modal.show();
+        })
+    })
+})
